f4a1f0f6b9b5b8ada7c2967bfc795d1ee8eb2712
Bamolitho <Bamolitho@users.noreply.github.com>
2025-11-06 13:23:27 -0500

    Initial commit: Flask hello microservice

--- app/app.py
+++ app/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/hello")
 def hello():
-    return {"message" : "Hello world from CI/CD!"}
+    return {"message": "Hello world from CI/CD!"}
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5801)

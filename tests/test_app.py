1ff20280254ac617cc3d175e2ed3b5efca6b8bc1
Bamolitho <Bamolitho@users.noreply.github.com>
2025-11-08 10:22:37 -0500

    Tutos complet CI/CD ici 👉 https://github.com/Bamolitho/InsideCI-CD

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,6 @@ def test_hello_route():
     client = app.test_client()
     response = client.get("/hello")
     assert response.status_code == 200
-    assert response.get_json() == {"message": "Hello world from CI/CD! - Phase 4 completed"}
+    assert response.get_json() == {
+        "message": "Hello world from CI/CD! - Phase 4 completed"
+    }
